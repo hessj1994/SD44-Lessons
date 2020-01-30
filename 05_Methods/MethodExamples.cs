49f895300d4daaf42070ce72ff732cc2dbfbb6be
aknight9255 <amanda.knight55@gmail.com>
2020-01-30 08:41:12 -0500

    Methods

--- 05_Methods/MethodExamples.cs
+++ 05_Methods/MethodExamples.cs
@@ -16,6 +16,25 @@ namespace _05_Methods
         {
             return numOne + numTwo;
         }
+        public int Add(int numOne, int numTwo,int another)
+        {
+            return numOne + numTwo + another;
+        }
+
+        public double Add(double numberOne, double numberTwo)
+        {
+            double sum = numberOne + numberTwo;
+            return sum;
+        }
+        private int CalculateAge(DateTime birthday)
+        {
+            TimeSpan ageSpan = DateTime.Now - birthday;
+            double totalAgeInYear = ageSpan.TotalDays / 365.25;
+            double floorAge = Math.Floor(totalAgeInYear);
+            int years = Convert.ToInt32(floorAge);
+            //int years = Convert.ToInt32(Math.Floor(totalAgeInYear));
+            return years;
+        }
 
         [TestMethod]
         public void MethodExecution()
@@ -25,6 +44,10 @@ namespace _05_Methods
 
             int total = Add(23,86);
             Console.WriteLine(total);
+            var another = Add(1,6);
+            DateTime birthdate = new DateTime(1944, 12, 24);
+            Console.WriteLine(CalculateAge(birthdate));
+            
         }
     }
 }
