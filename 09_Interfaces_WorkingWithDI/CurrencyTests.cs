4c7857f6935946cb1da84b032f00430e2b2a9baf
aknight9255 <amanda.knight55@gmail.com>
2020-02-06 17:05:35 -0500

    Interfaces tests and classes

---
+++ 09_Interfaces_WorkingWithDI/CurrencyTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _09_Interfaces_WorkingWithDI
+{
+    [TestClass]
+    public class CurrencyTests
+    {
+        [TestMethod]
+        public void PennyTests()
+        {
+            ICurrency penny = new Penny();
+            Assert.AreEqual(0.01m, penny.Value);
+            Assert.AreEqual("Penny", penny.Name);
+        }
+        [TestMethod]
+        public void NickelTests()
+        {
+            ICurrency nickel = new Nickel();
+            Assert.AreEqual(0.05m, nickel.Value);
+            Assert.AreEqual("Nickel", nickel.Name);
+        }
+        [TestMethod]
+        public void DollarTest()
+        {
+            ICurrency dollar = new Dollar();
+            Assert.AreEqual(1.0m, dollar.Value);
+            Assert.AreEqual("Dollar", dollar.Name);
+        }
+
+        [DataTestMethod]
+        [DataRow(100.2)]
+        [DataRow(37.12)]
+        [DataRow(2319.18)]
+        [DataRow(1408.0)]
+        public void EPaymentTest(double value)
+        {
+            decimal convertedValue = Convert.ToDecimal(value);
+            var epayment = new ElectronicPayment(convertedValue);
+            Assert.AreEqual(convertedValue, epayment.Value);
+            Assert.AreEqual("Electronic Payment", epayment.Name);
+        }
+    }
+}
---
+++ 09_Interfaces_WorkingWithDI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("09_Interfaces_WorkingWithDI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("09_Interfaces_WorkingWithDI")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80be7b71-10d4-460d-84f6-d4f949455596")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
